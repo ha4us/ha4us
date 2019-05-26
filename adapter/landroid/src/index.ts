f4a0a7f8c769e2c6176a7e4f8ea9c6f8d0941867
ulfalfa <ulf@steinberg-online.de>
2019-05-26 12:42:34 +0200

    feat(landroid): rewritten in typescript
    affects: @ha4us/landroid.adapter

---
+++ adapter/landroid/src/index.ts
@@ -0,0 +1,292 @@
+import { Subject, Subscription, from, of, never } from 'rxjs'
+
+import {
+  mergeMap,
+  switchMap,
+  map,
+  toArray,
+  delay,
+  catchError,
+  filter,
+  onErrorResumeNext,
+} from 'rxjs/operators'
+
+import {
+  ha4us,
+  StateService,
+  ObjectService,
+  CreateObjectMode,
+  DBMediaService,
+  MediaService,
+  // EventService,
+} from '@ha4us/adapter'
+import {
+  Ha4usError,
+  Ha4usRole,
+  MqttUtil,
+  Ha4usObject,
+  Ha4usObjectType,
+} from '@ha4us/core'
+
+import { Landroid } from './landroid'
+
+const ADAPTER_OPTIONS = {
+  name: 'landroid',
+  path: __dirname + '/..',
+  args: {
+    ip: {
+      demandOption: true,
+      describe: 'id of the landroid api',
+      type: 'string',
+      alias: 'ip',
+    },
+    pin: {
+      demandOption: true,
+      describe: 'pin for accessing the api',
+      type: 'string',
+      alias: 'pin',
+    },
+    poll: {
+      demandOption: false,
+      default: 5,
+      describe: 'polling intervall in sec.',
+      type: 'number',
+    },
+  },
+  imports: ['$log', '$args', '$states', '$objects'],
+}
+
+function Adapter(
+  $log,
+  $args,
+  $states: StateService,
+
+  $objects: ObjectService
+) {
+  let sub: Subscription
+  async function $onInit() {
+    //  `http://${$args.landroidIp}:80/jsondata.cgi`,
+
+    const landi = new Landroid($args.landroidIp, $args.landroidPin)
+
+    await $objects.install(
+      null,
+      { role: 'adapter/landroid' },
+      CreateObjectMode.create
+    )
+
+    await $objects.install(
+      'landroid',
+      { role: 'Device/Landroid' },
+      CreateObjectMode.create
+    )
+
+    await $objects.install(
+      'landroid/battery',
+      {
+        role: 'Value/System/Battery',
+        type: Ha4usObjectType.Number,
+        can: { read: false, write: false, trigger: true },
+      },
+      CreateObjectMode.create
+    )
+    await $objects.install(
+      'landroid/state',
+      {
+        role: 'Mode/Landroid/State',
+        type: Ha4usObjectType.String,
+        can: { read: false, write: false, trigger: true },
+      },
+      CreateObjectMode.create
+    )
+    await $objects.install(
+      'landroid/error',
+      {
+        role: 'Value/Landroid/Errortext',
+        type: Ha4usObjectType.String,
+        can: { read: false, write: false, trigger: true },
+      },
+      CreateObjectMode.create
+    )
+
+    await $objects.install(
+      'landroid/start',
+      {
+        role: 'Action/PressShort',
+        type: Ha4usObjectType.Boolean,
+        can: { read: false, write: true, trigger: true },
+      },
+      CreateObjectMode.create
+    )
+    await $objects.install(
+      'landroid/stop',
+      {
+        role: 'Action/PressShort',
+        type: Ha4usObjectType.Boolean,
+        can: { read: false, write: true, trigger: true },
+      },
+      CreateObjectMode.create
+    )
+
+    landi.observe().subscribe(data => {
+      $log.debug('Work req', data.data.workReq)
+      $states.status(
+        '$landroid/battery',
+        data.debug.landroid.battery.percentage,
+        true
+      )
+      $states.status('$landroid/state', data.debug.landroid.state, true)
+      $states.status(
+        '$landroid/error',
+        data.data.message !== 'none' ? data.data.message : '',
+        true
+      )
+    })
+
+    $states
+      .observe('/$set/landroid/start')
+      .pipe(filter(msg => msg.val === true || msg.val === 'true'))
+      .subscribe(msg => {
+        landi.start()
+      })
+
+    $states
+      .observe('/$set/landroid/stop')
+      .pipe(filter(msg => msg.val === true || msg.val === 'true'))
+      .subscribe(msg => {
+        landi.stop()
+      })
+
+    /* publish('distance', data.distance);
+    publish('battery/state', data.batteryChargerState );
+    publish('battery/percentage',debug.landroid.battery.percentage);
+    publish('work_request', data.workReq);
+    publish('error', data.message);
+    publish('firmware', data.versione_fw);
+    publish('waitAfterRain', data.rit_pioggia );
+    publish('rain', debug.landroid.rainSensor );
+    publish('area/total', data.num_aree_lavoro);
+    publish('area/current', data.area);
+    publish('area/computed', debug.landroid.aree.vet[debug.landroid.aree.index]);
+    publish('borderCut', data.enab_bordo === 1);*/
+
+    /*
+
+    await $objects.install(
+      'sun',
+      {
+        role: 'value/sunposition',
+        can: { read: false, write: false, trigger: true },
+      },
+      CreateObjectMode.create
+    )*/
+
+    /*  sub.add(
+      $states
+        .observe('/$set/+/state')
+        .pipe(
+          switchMap(msg => {
+            const [scriptName] = msg.match.params
+            $log.debug(`Setting state of ${scriptName} to ${msg.val}`)
+
+            const storedScript = scripts.get(
+              MqttUtil.join($args.name, scriptName)
+            )
+
+            return of(storedScript).pipe(
+              mergeMap(aScript => {
+                if (aScript) {
+                  if (msg.val === true) {
+                    return aScript.compile().then(() => aScript.start())
+                  } else {
+                    return aScript.stop()
+                  }
+                } else {
+                  throw new Ha4usError(
+                    404,
+                    `script ${scriptName} does not exists`
+                  )
+                }
+              }),
+              catchError(e => {
+                $log.error(
+                  `Error setting state of ${scriptName} to ${msg.val} because ${
+                    e.message
+                  }`
+                )
+                return never()
+              })
+            )
+          })
+        )
+        .subscribe(
+          (script: Ha4usScript) => {
+            $log.info(
+              `script ${script.name} is ${
+                script.running ? 'running' : 'stopped'
+              }`
+            )
+          },
+          e => {
+            $log.error(`BUMMER`, e)
+          },
+          () => {
+            $log.error('Script STATE Listener completed')
+          }
+        )
+    )*/
+
+    $states.connected = 2
+
+    return true
+  }
+
+  async function $onDestroy() {}
+
+  return {
+    $onInit,
+    $onDestroy,
+  }
+}
+
+ha4us(ADAPTER_OPTIONS, Adapter)
+
+/*function loadDir(dir) {
+    async function destroyScript(name: string): Promise<Ha4usScript> {
+      if (!scripts.has(name)) {
+        throw new Ha4usError(404, `script ${name} does not exist`)
+      }
+      const script = scripts.get(name)
+      $log.info('Stopping script', script.name)
+      // run cleanup
+      try {
+        script.destroy()
+      } catch (e) {
+        $log.error('Probles cleaning up script', e)
+        throw e
+      } finally {
+        scripts.delete(name)
+      }
+
+      return script
+    }
+
+    dir = path.resolve(dir)
+    $log.debug('Loading dir', dir)
+    const watch = chokidar.watch(dir + '/*.js')
+    watch.on('ready', () => {
+      $log.debug('Watching', watch.getWatched())
+    })
+    watch.on('add', (file: string, _?: fs.Stats) => {
+      $log.info('Loading and Run script', file)
+      installScript(new Ha4usScript(file), file)
+    })
+    watch.on('change', (file: string, _?: fs.Stats) => {
+      $log.info('Script %s changed', file)
+      //reloadScript(file)
+    })
+    watch.on('unlink', (file: string) => {
+      $log.info('Script %s deleted', file)
+      destroyScript(file)
+    })
+  }*/
---
+++ adapter/landroid/src/landroid.ts
@@ -0,0 +1,222 @@
+import axios from 'axios'
+import { interval, combineLatest } from 'rxjs'
+import { switchMap, combineAll, map } from 'rxjs/operators'
+export type LandroidState =
+  | 'home'
+  | 'start sequence'
+  | 'leaving house'
+  | 'grass cutting'
+  | 'trapped recovery'
+  | 'lift recovery'
+  | 'searching wire'
+  | 'searching home'
+  | 'following wire'
+  | 'idle'
+
+export type LandroidReq =
+  | 'scheduler req grass cut'
+  | 'landroid req manual homing'
+  | 'user req grass cut'
+
+export type LandroidBatState = 'idle' | 'charging'
+
+export type LandroidMessage =
+  | 'close door to cut grass'
+  | 'outside wire'
+  | 'trapped'
+  | 'wire missing'
+  | 'raining'
+  | 'none'
+
+export interface LandroidData {
+  versione_fw: number
+  lingua: number
+  ore_funz: number[]
+  ora_on: number[]
+  min_on: number[]
+  allarmi: number[]
+  /**
+   *  [0] "Blade blocked"
+   *  [1] "Repositioning error"
+   *  [2] "Outside wire" ("Outside working area")
+   *  [3] "Blade blocked"
+   *  [4] "Outside wire" ("Outside working area")
+   *  [5] "Mower lifted" ("Lifted up")
+   *  [6] "error"
+   *  [7] "error" (Set when "Lifted up" - "Upside down"?)
+   *  [8] "error"
+   *  [9] "Collision sensor blocked"
+   *  [10] "Mower tilted"
+   *  [11] "Charge error" (Set when "Lifted up"?)
+   *  [12] "Battery error"
+   **/
+
+  settaggi: number[]
+  mac: number[]
+
+  time_format: number
+  date_format: number
+  rit_pioggia: number
+  area: number
+  enab_bordo: number
+  percent_programmatore: number
+  indice_area: number
+  tempo_frenatura: number
+  perc_rallenta_max: number
+  canale: number
+  num_ricariche_batt: number
+  num_aree_lavoro: number
+  dist_area: number[]
+  perc_per_area: number[]
+  area_in_lavoro: number
+  email: string
+  perc_batt: string
+  ver_proto: number
+  state: LandroidState
+  workReq: LandroidReq
+  message: LandroidMessage
+  batteryChargerState: LandroidBatState
+  distance: number
+}
+
+export interface LandroidDebug {
+  landroid: {
+    state: LandroidState
+    boardTemperature: number
+    distance: number
+    wheelLeftDistance: number
+    wheelRightDistance: number
+    angle: number
+    rainSensor: number
+    aree: { index: number; vet: number[] }
+    battery: {
+      percentage: number
+      voltage: number
+      temperature: number
+      ntcResistance: number
+    }
+    batteryCharger: { state: LandroidBatState; chargeCurrent: number }
+    accelerometer: { gravity: number[]; angle: number[] }
+    gyroscope: { angularSpeed: number[]; angle: number[] }
+    motor: [
+      {
+        speed: number
+        maxSpeed: number
+        speedReduction: number
+        rpm: number
+        feedbackError: number
+        acceleration: number
+        deceleration: number
+        fault: boolean
+      },
+      {
+        speed: number
+        maxSpeed: number
+        speedReduction: number
+        rpm: number
+        feedbackError: number
+        acceleration: number
+        deceleration: number
+        fault: boolean
+      },
+      {
+        speed: number
+        maxSpeed: number
+        speedReduction: number
+        rpm: number
+        feedbackError: number
+        acceleration: number
+        deceleration: number
+        fault: boolean
+      }
+    ]
+    guide: {
+      straightSpeed: number
+      turningSpeed: number
+      measuredWheelDeltaDistance: number
+      requiredWheelDeltaDistance: number
+      deltaSpeedCorrection: number
+    }
+  }
+  id: {
+    stop1: boolean
+    stop2: boolean
+    lift1: boolean
+    lift2: boolean
+    trappedLeft: boolean
+    trappedRight: boolean
+    door1: boolean
+    door2: boolean
+  }
+
+  dipSw: { sw1: boolean; sw2: boolean; sw3: boolean; sw4: boolean }
+  wireSensor: {
+    fwVer: number
+    left: 'inside' | 'outside'
+    right: 'outside' | 'inside'
+  }
+}
+
+export class Landroid {
+  constructor(protected ip: string, protected pin: string) {}
+
+  async readDebug(): Promise<LandroidDebug> {
+    return this.doGet('Debug')
+  }
+
+  async readData(): Promise<LandroidData> {
+    return this.doGet('data')
+  }
+
+  async doGet<T>(what: 'Debug' | 'data'): Promise<T> {
+    return axios
+      .get<T>(`http://${this.ip}:80/json${what}.cgi`, {
+        auth: {
+          username: 'admin',
+          password: this.pin,
+        },
+      })
+      .then(data => {
+        return data.data
+      })
+      .catch(e => {
+        if (e.response.status === 404) {
+          throw new Error(`landroid not found (${this.ip})`)
+        }
+        throw e
+      })
+  }
+
+  async doPost(request: string) {
+    return axios
+      .post<LandroidData>(`http://${this.ip}:80/jsondata.cgi`, request, {
+        headers: {
+          'content-length': request.length,
+          'content-type': 'application/x-www-form-urlencoded',
+          Accept: 'application/json',
+        },
+        auth: {
+          username: 'admin',
+          password: this.pin,
+        },
+      })
+      .then(result => result.data.state)
+  }
+  start() {
+    return this.doPost('data=[["settaggi", 11, 1]]')
+  }
+
+  stop() {
+    return this.doPost('data=[["settaggi", 12, 1]]')
+  }
+
+  observe(anInterval = 5) {
+    return interval(anInterval * 1000).pipe(
+      switchMap(() => {
+        return combineLatest([this.readData(), this.readDebug()]).pipe(
+          map(data => ({ data: data[0], debug: data[1] }))
+        )
+      })
+    )
+  }
+}
